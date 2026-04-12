75a7df3d7cae45886f98af17e928ce6153f98ff9
XyloIsCoding <xylo.is.coding@gmail.com>
2026-04-13 01:14:46 +0200

    added jump static library (no functions yet)

---
+++ Source/XyloMovementUtil/Public/ModularMovement/XMoveU_JumpStaticLibrary.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2026, XyloIsCoding. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "XMoveU_JumpStaticLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XYLOMOVEMENTUTIL_API UXMoveU_JumpStaticLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	
+};
