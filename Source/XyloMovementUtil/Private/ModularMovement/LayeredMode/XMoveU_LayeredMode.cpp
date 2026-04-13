4da0e88ab30be1b60b9b78efa858aa020bd9ece1
XyloIsCoding <xylo.is.coding@gmail.com>
2026-04-13 18:34:14 +0200

    added layered mode class

---
+++ Source/XyloMovementUtil/Private/ModularMovement/LayeredMode/XMoveU_LayeredMode.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2026, XyloIsCoding. All rights reserved.
+
+
+#include "ModularMovement/LayeredMode/XMoveU_LayeredMode.h"
---
+++ Source/XyloMovementUtil/Public/ModularMovement/LayeredMode/XMoveU_LayeredMode.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2026, XyloIsCoding. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "XMoveU_LayeredMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable, DefaultToInstanced, EditInlineNew, Abstract)
+class XYLOMOVEMENTUTIL_API UXMoveU_LayeredMode : public UObject
+{
+	GENERATED_BODY()
+
+	
+};
