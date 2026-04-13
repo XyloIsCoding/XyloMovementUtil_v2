cde66683a6c35cdfc0721a7aa0f822adde7705ce
XyloIsCoding <xylo.is.coding@gmail.com>
2026-04-13 19:08:25 +0200

    added constructor and owner getter to XMoveU_LayeredMode

--- Source/XyloMovementUtil/Private/ModularMovement/LayeredMode/XMoveU_LayeredMode.cpp
+++ Source/XyloMovementUtil/Private/ModularMovement/LayeredMode/XMoveU_LayeredMode.cpp
@@ -2,3 +2,22 @@
 
 
 #include "ModularMovement/LayeredMode/XMoveU_LayeredMode.h"
+
+#include "ModularMovement/XMoveU_ModularCharacter.h"
+#include "ModularMovement/XMoveU_ModularMovementComponent.h"
+
+UXMoveU_LayeredMode::UXMoveU_LayeredMode(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+UXMoveU_ModularMovementComponent* UXMoveU_LayeredMode::GetOwningMoveComp() const
+{
+	return Cast<UXMoveU_ModularMovementComponent>(GetOuter());
+}
+
+AXMoveU_ModularCharacter* UXMoveU_LayeredMode::GetOwningCharacter() const
+{
+	UXMoveU_ModularMovementComponent* MoveComp = GetOwningMoveComp();
+	return MoveComp ? Cast<AXMoveU_ModularCharacter>(MoveComp->GetCharacterOwner()) : nullptr;
+}
--- Source/XyloMovementUtil/Public/ModularMovement/LayeredMode/XMoveU_LayeredMode.h
+++ Source/XyloMovementUtil/Public/ModularMovement/LayeredMode/XMoveU_LayeredMode.h
@@ -6,6 +6,9 @@
 #include "UObject/Object.h"
 #include "XMoveU_LayeredMode.generated.h"
 
+class AXMoveU_ModularCharacter;
+class UXMoveU_ModularMovementComponent;
+
 /**
  * 
  */
@@ -13,6 +16,18 @@ UCLASS(BlueprintType, Blueprintable, DefaultToInstanced, EditInlineNew, Abstract
 class XYLOMOVEMENTUTIL_API UXMoveU_LayeredMode : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	UXMoveU_LayeredMode(const FObjectInitializer& ObjectInitializer);
+protected:
+	UXMoveU_ModularMovementComponent* GetOwningMoveComp() const;
+	AXMoveU_ModularCharacter* GetOwningCharacter() const;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * UXMoveU_LayeredMode
+	 */
 
 	
 };
