1f0aae5f0957b2704036cf176a7bb86d955654eb
XyloIsCoding <xylo.is.coding@gmail.com>
2026-04-14 20:35:09 +0200

    added sprint layered movement mode

---
+++ Source/XyloMovementUtil/Private/ModularMovement/LayeredMovementMode/Preset/XMoveU_SprintLayeredMoveMode.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) 2026, XyloIsCoding. All rights reserved.
+
+
+#include "ModularMovement/LayeredMovementMode/Preset/XMoveU_SprintLayeredMoveMode.h"
+
+#include "ModularMovement/XMoveU_ModularMovementComponent.h"
+
+UXMoveU_SprintLayeredMoveMode::UXMoveU_SprintLayeredMoveMode(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	MaxWalkSpeedSprinting = 570.f;
+	MinSprintingAngleCosine = 0.5f;
+}
+
+void UXMoveU_SprintLayeredMoveMode::ModifyMaxSpeed(float& OutMaxSpeed) const
+{
+	UXMoveU_ModularMovementComponent* MoveComp = GetOwningMoveComp();
+	if (MoveComp && MoveComp->IsWalking())
+	{
+		OutMaxSpeed = MaxWalkSpeedSprinting;
+	}
+}
+
+bool UXMoveU_SprintLayeredMoveMode::ShouldEnterMode(float DeltaSeconds) const
+{
+	return !IsInMode() && WantsToBeInMode() && CanSprintInCurrentState();
+}
+
+bool UXMoveU_SprintLayeredMoveMode::ShouldLeaveMode(float DeltaSeconds) const
+{
+	return IsInMode() && (!WantsToBeInMode() || !CanSprintInCurrentState());
+}
+
+bool UXMoveU_SprintLayeredMoveMode::ShouldForceLeaveMode(float DeltaSeconds) const
+{
+	return IsInMode() && !CanSprintInCurrentState();
+}
+
+bool UXMoveU_SprintLayeredMoveMode::CanSprintInCurrentState() const
+{
+	UXMoveU_ModularMovementComponent* MoveComp = GetOwningMoveComp();
+	if (!MoveComp || !MoveComp->UpdatedComponent || MoveComp->UpdatedComponent->IsSimulatingPhysics())
+	{
+		return false;
+	}
+	
+	if ((MoveComp->GetCurrentAcceleration().GetSafeNormal() | MoveComp->UpdatedComponent->GetForwardVector()) < MinSprintingAngleCosine) return false;
+	return (MoveComp->IsFalling() || MoveComp->IsMovingOnGround()) && !MoveComp->IsCrouching();
+}
---
+++ Source/XyloMovementUtil/Public/ModularMovement/LayeredMovementMode/Preset/XMoveU_SprintLayeredMoveMode.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2026, XyloIsCoding. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ModularMovement/LayeredMovementMode/XMoveU_LayeredMovementMode.h"
+#include "XMoveU_SprintLayeredMoveMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XYLOMOVEMENTUTIL_API UXMoveU_SprintLayeredMoveMode : public UXMoveU_LayeredMovementMode
+{
+	GENERATED_BODY()
+
+public:
+	UXMoveU_SprintLayeredMoveMode(const FObjectInitializer& ObjectInitializer);
+
+public:
+	virtual void ModifyMaxSpeed(float& OutMaxSpeed) const override;
+	
+public:
+	virtual bool ShouldEnterMode(float DeltaSeconds) const override;
+	virtual bool ShouldLeaveMode(float DeltaSeconds) const override;
+	virtual bool ShouldForceLeaveMode(float DeltaSeconds) const override;
+protected:
+	virtual bool CanSprintInCurrentState() const;
+
+protected:
+	/** The maximum ground speed when walking and Sprinting. */
+	UPROPERTY(Category="Sprinting", EditAnywhere, BlueprintReadWrite, meta=(ClampMin="0", UIMin="0", ForceUnits="cm/s"))
+	float MaxWalkSpeedSprinting;
+
+	/** The cosine of the maximum angle sprint is allowed. */
+	UPROPERTY(Category="Sprinting", EditAnywhere, BlueprintReadWrite, meta=(ClampMin="0", UIMin="0"))
+	float MinSprintingAngleCosine;
+};
